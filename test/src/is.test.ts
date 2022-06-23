daaa89d34a5fdb7593e3afae4e15a2e5f1c1e417
xiaoxunyao <huchengyea@163.com>
2022-06-23 13:29:11 +0800

    test: add function test

--- test/src/is.test.ts
+++ test/src/is.test.ts
@@ -2,7 +2,31 @@ import { expect, test } from 'vitest'
 
 import is from '@elonehoo/is'
 
-test('is',()=>{
+test('is undefined',()=>{
+  expect(is.undefined(undefined)).toBe(true)
+})
+
+test('is null',()=>{
+  expect(is.null_(null)).toBe(true)
+})
+
+test('is string',()=>{
   expect(is('a')).toBe('string')
   expect(is.string('a')).toBe(true)
 })
+
+test('is number',()=>{
+  expect(is.number(1)).toBe(true)
+})
+
+test('is boolean',()=>{
+  expect(is.boolean(true)).toBe(true)
+})
+
+test('is numericString',()=>{
+  expect(is.numericString('')).toBe(false)
+  expect(is.numericString(' ')).toBe(false)
+  expect(is.numericString(' \t\t\n')).toBe(false)
+  expect(is.numericString(1)).toBe(false)
+})
+
