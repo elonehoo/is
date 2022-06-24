205b2b1fca606f2284d7f56597ec32f36326423f
xiaoxunyao <huchengyea@163.com>
2022-06-24 12:55:39 +0800

    test: end test

--- test/src/is.test.ts
+++ test/src/is.test.ts
@@ -32,5 +32,39 @@ test('is numericString',()=>{
 
 test('is array',()=>{
   expect(is.array([1, 2, 3])).toBe(true)
+  expect(is.array([1, '2', 3])).toBe(true)
 })
 
+test('is boundFunction',()=>{
+  expect(is.boundFunction(function () {})).toBe(false)
+})
+
+test('is urlInstance',()=>{
+  const url = new URL('https://elonehoo.xyz/')
+  expect(is.urlInstance(url)).toBe(true)
+  expect(is.urlInstance(new URL(url))).toBe(true)
+  expect(is.urlString({})).toBe(false)
+  expect(is.urlString(undefined)).toBe(false)
+  expect(is.urlString(null)).toBe(false)
+})
+
+test('is truthy',()=>{
+  expect(is.truthy('unicorn')).toBe(true)
+  expect(is.truthy('🐦')).toBe(true)
+  expect(is.truthy(new Set())).toBe(true)
+  expect(is.truthy(Symbol('🐦'))).toBe(true)
+  expect(is.truthy(true)).toBe(true)
+  expect(is.truthy(1)).toBe(true)
+  expect(is.truthy(BigInt(1))).toBe(true)
+})
+
+test('is falsy',()=>{
+  expect(is.falsy(false)).toBe(true)
+  expect(is.falsy(0)).toBe(true)
+  expect(is.falsy('')).toBe(true)
+  expect(is.falsy(null)).toBe(true)
+  expect(is.falsy(undefined)).toBe(true)
+  expect(is.falsy(Number.NaN)).toBe(true)
+  expect(is.falsy(0n)).toBe(true)
+  expect(is.falsy(BigInt(0))).toBe(true)
+})
